ae0ce58df971f5a9f1c0bed4e2d2e4380aa0e15f
Eugênio Moreira <eugeniojava@outlook.com>
2021-09-14 16:59:14 -0300

    feat(spring-batch): setup project

---
+++ curso-para-desenvolvimento-de-jobs-com-spring-batch/first-spring-batch-project/src/main/java/com/eugeniojava/firstspringbatchproject/FirstSpringBatchProjectApplication.java
@@ -0,0 +1,12 @@
+package com.eugeniojava.firstspringbatchproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBatchProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringBatchProjectApplication.class, args);
+    }
+}
