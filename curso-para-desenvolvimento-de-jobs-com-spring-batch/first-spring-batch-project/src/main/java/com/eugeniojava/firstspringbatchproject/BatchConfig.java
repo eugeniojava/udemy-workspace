6e35ee16a759c8116f7860c222e8397c409f008d
Eugênio Moreira <eugeniojava@outlook.com>
2021-09-14 16:59:39 -0300

    feat(spring-batch): implement first job

---
+++ curso-para-desenvolvimento-de-jobs-com-spring-batch/first-spring-batch-project/src/main/java/com/eugeniojava/firstspringbatchproject/BatchConfig.java
@@ -0,0 +1,39 @@
+package com.eugeniojava.firstspringbatchproject;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class BatchConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public BatchConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Job printWelcomeMessageJob() {
+        return jobBuilderFactory.get("printWelcomeMessageJob")
+                .start(printWelcomeMessageStep())
+                .build();
+    }
+
+    public Step printWelcomeMessageStep() {
+        return stepBuilderFactory.get("printWelcomeMessageStep")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("Hello, world!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
