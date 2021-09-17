56ab8ccf127ccf25ba1c93bb14eb55cfba939e2f
Eugênio Moreira <eugeniojava@outlook.com>
2021-09-17 15:24:14 -0300

    feat(spring-batch): work with multiple executions and scope

--- curso-para-desenvolvimento-de-jobs-com-spring-batch/first-spring-batch-project/src/main/java/com/eugeniojava/firstspringbatchproject/BatchConfig.java
+++ curso-para-desenvolvimento-de-jobs-com-spring-batch/first-spring-batch-project/src/main/java/com/eugeniojava/firstspringbatchproject/BatchConfig.java
@@ -5,7 +5,11 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,15 +29,22 @@ public class BatchConfig {
     public Job printWelcomeMessageJob() {
         return jobBuilderFactory.get("printWelcomeMessageJob")
                 .start(printWelcomeMessageStep())
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
-    public Step printWelcomeMessageStep() {
+    private Step printWelcomeMessageStep() {
         return stepBuilderFactory.get("printWelcomeMessageStep")
-                .tasklet((stepContribution, chunkContext) -> {
-                    System.out.println("Hello, world!");
-                    return RepeatStatus.FINISHED;
-                })
+                .tasklet(printWelcomeMessageTasklet(null))
                 .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet printWelcomeMessageTasklet(@Value("#{jobParameters['name']}") String name) {
+        return (stepContribution, chunkContext) -> {
+            System.out.printf("Hello, %s!%n", name);
+            return RepeatStatus.FINISHED;
+        };
     }
 }
