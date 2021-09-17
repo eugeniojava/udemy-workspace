073d91b08077c3441f4f737bf40e858edf26e1d3
Eugênio Moreira <eugeniojava@outlook.com>
2021-09-17 14:38:58 -0300

    feat(spring-batch): persist metadata in the database

--- curso-para-desenvolvimento-de-jobs-com-spring-batch/first-spring-batch-project/src/main/java/com/eugeniojava/firstspringbatchproject/BatchConfig.java
+++ curso-para-desenvolvimento-de-jobs-com-spring-batch/first-spring-batch-project/src/main/java/com/eugeniojava/firstspringbatchproject/BatchConfig.java
@@ -9,8 +9,8 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@EnableBatchProcessing
 @Configuration
+@EnableBatchProcessing
 public class BatchConfig {
 
     private final JobBuilderFactory jobBuilderFactory;
